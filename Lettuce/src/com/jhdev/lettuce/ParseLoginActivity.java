d5a284bbd9acc633319ae06a53992759ca480dbd
Julien Ho <jianhui@gmail.com>
2014-04-06 17:06:41 +0800

    so far so bad

--- Lettuce/src/com/jhdev/lettuce/ParseLoginActivity.java
+++ Lettuce/src/com/jhdev/lettuce/ParseLoginActivity.java
@@ -3,34 +3,59 @@ package com.jhdev.lettuce;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.os.Build;
 
 public class ParseLoginActivity extends Activity {
+	
+	Button btn_LoginIn = null;
+	Button btn_SignUp = null;
+	Button btn_ForgetPass = null;
+	private EditText mUserNameEditText;
+	private EditText mPasswordEditText;
+
+	// flag for Internet connection status
+	Boolean isInternetPresent = false;
+	// Connection detector class
+	ConnectionDetector cd;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_parselogin);
 
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+//		if (savedInstanceState == null) {
+//			getFragmentManager().beginTransaction()
+//				.add(R.id.container, new PlaceholderFragment()).commit();
+//		}
+		
+		// creating connection detector class instance
+		cd = new ConnectionDetector(getApplicationContext());
+
+		btn_LoginIn = (Button) findViewById(R.id.btn_login);
+		btn_SignUp = (Button) findViewById(R.id.btn_signup);
+		btn_ForgetPass = (Button) findViewById(R.id.btn_ForgetPass);
+		mUserNameEditText = (EditText) findViewById(R.id.username);
+		mPasswordEditText = (EditText) findViewById(R.id.password);
+
+		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.test2, menu);
+		getMenuInflater().inflate(R.menu.parse_login, menu);
 		return true;
 	}
 
